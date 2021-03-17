89451859176322a787f10054ef8e109b4c5bc321
Jesse Kramer <jesse@kramerventures.nl>
2021-03-17 11:26:55 +0100

    Codestyle

--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -3,15 +3,11 @@
 namespace DnsMadeEasy\Tests\Unit;
 
 use DnsMadeEasy\Client;
-use DnsMadeEasy\Interfaces\Managers\AbstractManagerInterface;
-use DnsMadeEasy\Interfaces\PaginatorFactoryInterface;
 use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase;
-use Psr\Http\Client\ClientInterface as HttpClientInterface;
 
 class ClientTest extends TestCase
 {
-
     public function testCreateClient(): void
     {
         $client = new Client();
