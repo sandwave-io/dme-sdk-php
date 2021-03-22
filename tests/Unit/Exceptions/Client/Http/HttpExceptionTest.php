73fa1f554bc139d6e27b3380eb586119e4c9f5a5
Jesse Kramer <jesse@kramerventures.nl>
2021-03-22 12:16:37 +0100

    Add test

---
+++ tests/Unit/Exceptions/Client/Http/HttpExceptionTest.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace DnsMadeEasy\Tests\Unit\Exceptions\Client\Http;
+
+use DnsMadeEasy\Exceptions\Client\Http\HttpException;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+class HttpExceptionTest extends TestCase
+{
+    public function testSettersAndGetters(): void
+    {
+        $exception = new HttpException();
+        Assert::assertNull($exception->getRequest(), 'request should not be set');
+        Assert::assertNull($exception->getResponse(), 'response should not be set');
+
+        $exception->setRequest(new Request('GET', '/test'));
+        Assert::assertNotNull($exception->getRequest(), 'request should be set');
+        Assert::assertSame('GET', $exception->getRequest()->getMethod(), 'request should be the same as has been set');
+        Assert::assertSame('/test', $exception->getRequest()->getUri()->getPath(), 'request should be the same as has been set');
+
+        $exception->setResponse(new Response(200));
+        Assert::assertNotNull($exception->getResponse(), 'response should be set');
+        Assert::assertSame(200, $exception->getResponse()->getStatusCode(), 'response should be the same as has been set');
+    }
+}
